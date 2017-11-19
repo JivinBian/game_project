5e27f9a37d11d7f1ddf0f8cd5c285bc2e773da81
jivin <bjwbjw@126.com>
2017-11-19 23:03:26 +0800

    no message

--- Assets/GameCore/Script/Common/ObjectInput/PlayerController.cs
+++ Assets/GameCore/Script/Common/ObjectInput/PlayerController.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GameCore.Script.GameManagers.Log;
 using GameCore.Script.Interface;
 using GameCore.Script.Managers.Object;
+using GameCore.Script.SceneObject;
 
 public class PlayerController : MonoBehaviour,IPlayerController
 {
     public ETCJoystick _joystick;
+    private Player _self;
     void Start()
     {
-        ObjectManager.GetInstance().Self.InitController(this);
+        _self=ObjectManager.GetInstance().Self;//.InitController(this);
+        _joystick.onMove.AddListener(OnMove);
     }
 
+    private void OnMove(Vector2 pVector2)
+    {
+        Vector3 pPos = _self.GetPosition();
+        _self.MoveTo( pPos+ new Vector3(pVector2.x, pPos.y, pVector2.y));
+        LogManager.Debug(pVector2);
+    }
     public void SetControlledTranform(Transform pTransform)
     {
-        _joystick.axisX.directTransform = pTransform;
-        _joystick.axisY.directTransform = pTransform;
+//        _joystick.axisX.directTransform = pTransform;
+//        _joystick.axisY.directTransform = pTransform;
     }
 }
